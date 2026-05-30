06fe87dd2e9beb189907e8b3276cea70c21bf4c0
SandeHuginn <igorkiller654@gmail.com>
2026-05-30 20:57:35 -0300

    Implementamos os teste para os relatorios usando PyTest

--- test_techflow.py
+++ test_techflow.py
@@ -148,3 +148,121 @@ def test_admin_pode_excluir(monkeypatch):
     sistema.excluir_tarefa()
 
     assert len(sistema.tarefas) == 0
+
+    # ---------------- TESTE RELATORIO DE DESEMPENHO/GERAL----------------
+def test_relatorio_desempenho(capsys):
+
+    sistema = Sistema()
+
+    sistema.usuario_logado = Usuario(
+        "admin",
+        "123",
+        "Administrador",
+        "admin"
+    )
+
+    tarefa1 = Tarefa(
+        1,
+        "Bug Login",
+        "Corrigir autenticação",
+        "Alta"
+    )
+    tarefa1.status = "Concluído"
+
+    tarefa2 = Tarefa(
+        2,
+        "Dashboard",
+        "Criar dashboard",
+        "Crítica"
+    )
+    tarefa2.status = "Em Andamento"
+
+    sistema.tarefas.append(tarefa1)
+    sistema.tarefas.append(tarefa2)
+
+    sistema.relatorio_desempenho()
+
+    saida = capsys.readouterr().out
+
+    assert "Total de tarefas: 2" in saida
+    assert "Tarefas concluídas: 1" in saida
+    assert "Tarefas em andamento: 1" in saida
+    assert "Tarefas críticas: 1" in saida
+
+    # ---------------- TESTE RELATORIO POR USUARIO----------------
+def test_relatorio_usuarios(capsys):
+
+    sistema = Sistema()
+
+    admin = Usuario(
+        "admin",
+        "123",
+        "Administrador",
+        "admin"
+    )
+
+    joao = Usuario(
+        "joao",
+        "123",
+        "João",
+        "local"
+    )
+
+    maria = Usuario(
+        "maria",
+        "123",
+        "Maria",
+        "local"
+    )
+
+    sistema.usuario_logado = admin
+
+    sistema.usuarios.append(admin)
+    sistema.usuarios.append(joao)
+    sistema.usuarios.append(maria)
+
+    tarefa1 = Tarefa(1, "Tarefa 1", "Descrição", "Alta")
+    tarefa1.responsavel = "joao"
+    tarefa1.status = "Concluído"
+
+    tarefa2 = Tarefa(2, "Tarefa 2", "Descrição", "Alta")
+    tarefa2.responsavel = "joao"
+    tarefa2.status = "Backlog"
+
+    tarefa3 = Tarefa(3, "Tarefa 3", "Descrição", "Alta")
+    tarefa3.responsavel = "maria"
+    tarefa3.status = "Concluído"
+
+    sistema.tarefas.extend([
+        tarefa1,
+        tarefa2,
+        tarefa3
+    ])
+
+    sistema.relatorio_usuarios()
+
+    saida = capsys.readouterr().out
+
+    assert "João" in saida
+    assert "Maria" in saida
+
+    assert "Total de tarefas: 2" in saida
+    assert "Concluídas: 1" in saida
+
+    # ---------------- TESTE RELATORIO VISIBILIDADE----------------
+def test_local_nao_pode_ver_relatorio(capsys):
+
+    sistema = Sistema()
+
+    sistema.usuario_logado = Usuario(
+        "joao",
+        "123",
+        "João",
+        "local"
+    )
+
+    sistema.relatorio_desempenho()
+
+    saida = capsys.readouterr().out
+
+    assert "Acesso negado!" in saida
